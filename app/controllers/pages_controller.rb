c36b4d5e0dfba4813b230b6be5649ca262a62df0
ezashi <soma3070@icloud.com>
2025-10-04 15:26:28 +0900

    feat: ホームページのロゴとスライドショー機能を改善
    - 新しいロゴ画像（usagiya.png）とロゴロゴ画像（usagiya_logo.png）を追加
    - ホームページのロゴをリンクに変更し、クリック時に指定のURLを開くように設定
    - スライドショーのクリック可能なスライドにイベントリスナーを追加し、クリック時に新しいタブでリンクを開くように修正
    - CSSスタイルを調整し、ロゴの表示やスライドの動作を改善

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,12 @@
 class PagesController < ApplicationController
+  layout false, only: [:home]
+
   def home
-    # トップ画面用のデータを取得
     @featured_products = Product.published.featured.limit(10)
     @seasonal_products = Product.published.seasonal.limit(10)
     @recent_notices = Notice.published.limit(3)
   end
 
   def philosophy
-    # 御菓子処うさぎやの想い
   end
 end
