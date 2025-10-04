b921d465c031fcf99f087307ea2d48004909321b
ezashi <soma3070@icloud.com>
2025-10-04 14:17:16 +0900

    feat: ホームページのスタイルとスライドショー機能を改善
    - home.html.erb:
      - 不要なJavaScriptインポートを削除
      - ハンバーガーメニューのホバー時のスタイルを改善
      - 店舗情報セクションのレイアウトを変更し、情報を整理
      - スライドショーの初期化処理を改善し、クリックイベントを追加
      - SNSリンクを新たに追加し、電話番号リンクを作成
    
    - routes.rb:
      - 管理者関連のルーティングをコメントアウト

--- config/routes.rb
+++ config/routes.rb
@@ -33,124 +33,124 @@ Rails.application.routes.draw do
     end
   end
 
-  # 管理者認証
-  devise_for :admin_users, path: "admin", controllers: {
-    sessions: "admin/sessions"
-  }
+  # # 管理者認証
+  # devise_for :admin_users, path: "admin", controllers: {
+  #   sessions: "admin/sessions"
+  # }
 
-  # 管理者用ページ
-  namespace :admin do
-    root "dashboard#index"
+  # # 管理者用ページ
+  # namespace :admin do
+  #   root "dashboard#index"
 
-    # 商品管理
-    resources :products do
-      member do
-        patch :publish
-        patch :unpublish
-        patch :add_to_featured
-        patch :remove_from_featured
-        patch :add_to_seasonal
-        patch :remove_from_seasonal
-        patch :update_featured_order
-        patch :update_seasonal_order
-      end
-      collection do
-        patch :bulk_update_featured_order
-        patch :bulk_update_seasonal_order
-      end
-    end
+  #   # 商品管理
+  #   resources :products do
+  #     member do
+  #       patch :publish
+  #       patch :unpublish
+  #       patch :add_to_featured
+  #       patch :remove_from_featured
+  #       patch :add_to_seasonal
+  #       patch :remove_from_seasonal
+  #       patch :update_featured_order
+  #       patch :update_seasonal_order
+  #     end
+  #     collection do
+  #       patch :bulk_update_featured_order
+  #       patch :bulk_update_seasonal_order
+  #     end
+  #   end
 
-    # おすすめ商品管理
-    resource :featured_products, only: [ :show, :update ] do
-      get :preview
-    end
+  #   # おすすめ商品管理
+  #   resource :featured_products, only: [ :show, :update ] do
+  #     get :preview
+  #   end
 
-    # 季節限定商品管理
-    resource :seasonal_products, only: [ :show, :update ] do
-      get :preview
-    end
+  #   # 季節限定商品管理
+  #   resource :seasonal_products, only: [ :show, :update ] do
+  #     get :preview
+  #   end
 
-    # お知らせ管理
-    resources :notices do
-      member do
-        patch :publish
-        patch :unpublish
-        get :preview
-      end
-    end
+  #   # お知らせ管理
+  #   resources :notices do
+  #     member do
+  #       patch :publish
+  #       patch :unpublish
+  #       get :preview
+  #     end
+  #   end
 
-    # 営業カレンダー管理
-    resources :calendar_events do
-      member do
-        patch :publish
-        patch :unpublish
-        get :preview
-      end
-    end
+  #   # 営業カレンダー管理
+  #   resources :calendar_events do
+  #     member do
+  #       patch :publish
+  #       patch :unpublish
+  #       get :preview
+  #     end
+  #   end
 
-    # お問い合わせ管理
-    resources :inquiries, only: [ :index, :show ] do
-      member do
-        patch :mark_as_read
-        patch :mark_as_unread
-      end
-    end
+  #   # お問い合わせ管理
+  #   resources :inquiries, only: [ :index, :show ] do
+  #     member do
+  #       patch :mark_as_read
+  #       patch :mark_as_unread
+  #     end
+  #   end
 
-    # 注文管理
-    resources :orders, only: [ :index, :show, :update ] do
-      member do
-        patch :update_status
-      end
-    end
-  end
+  #   # 注文管理
+  #   resources :orders, only: [ :index, :show, :update ] do
+  #     member do
+  #       patch :update_status
+  #     end
+  #   end
+  # end
 
-  # API endpoints for React
-  namespace :api do
-    namespace :v1 do
-      resources :products, only: [ :index, :show ]
-      resources :notices, only: [ :index, :show ]
-      resources :calendar_events, only: [ :index ]
-      resources :inquiries, only: [ :create ]
-      resources :orders, only: [ :create ]
+  # # API endpoints for React
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :products, only: [ :index, :show ]
+  #     resources :notices, only: [ :index, :show ]
+  #     resources :calendar_events, only: [ :index ]
+  #     resources :inquiries, only: [ :create ]
+  #     resources :orders, only: [ :create ]
 
-      namespace :admin do
-        resources :products do
-          member do
-            patch :publish
-            patch :unpublish
-            patch :add_to_featured
-            patch :remove_from_featured
-            patch :add_to_seasonal
-            patch :remove_from_seasonal
-          end
-          collection do
-            patch :bulk_update_featured_order
-            patch :bulk_update_seasonal_order
-          end
-        end
+  #     namespace :admin do
+  #       resources :products do
+  #         member do
+  #           patch :publish
+  #           patch :unpublish
+  #           patch :add_to_featured
+  #           patch :remove_from_featured
+  #           patch :add_to_seasonal
+  #           patch :remove_from_seasonal
+  #         end
+  #         collection do
+  #           patch :bulk_update_featured_order
+  #           patch :bulk_update_seasonal_order
+  #         end
+  #       end
 
-        resources :notices do
-          member do
-            patch :publish
-            patch :unpublish
-          end
-        end
+  #       resources :notices do
+  #         member do
+  #           patch :publish
+  #           patch :unpublish
+  #         end
+  #       end
 
-        resources :calendar_events do
-          member do
-            patch :publish
-            patch :unpublish
-          end
-        end
+  #       resources :calendar_events do
+  #         member do
+  #           patch :publish
+  #           patch :unpublish
+  #         end
+  #       end
 
-        resources :inquiries, only: [ :index, :show ] do
-          member do
-            patch :mark_as_read
-          end
-        end
+  #       resources :inquiries, only: [ :index, :show ] do
+  #         member do
+  #           patch :mark_as_read
+  #         end
+  #       end
 
-        resources :orders, only: [ :index, :show, :update ]
-      end
-    end
-  end
+  #       resources :orders, only: [ :index, :show, :update ]
+  #     end
+  #   end
+  # end
 end
