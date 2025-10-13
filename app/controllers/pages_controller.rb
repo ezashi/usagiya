43f75f727bfaa3b5b037413fb1ee6a5ffcff40de
ezashi <soma3070@icloud.com>
2025-10-14 00:07:13 +0900

    feat: ヘッダーとスライドショー機能の改善
    - 不要なヘッダーのレンダリングを削除しました（pages_controller.rb, notices/index.html.erb, orders/new.html.erb, products/featured.html.erb, products/seasonal.html.erb, products/show.html.erb）。
    - スライドショーのクリック可能なスライドのスタイルを改善し、他のスライドは通常のカーソルに変更しました（pages/home.html.erb）。
    - サイドメニューとオーバーレイの表示ロジックをTurboに対応させ、初回読み込み時とページ遷移時にメニューを正しく初期化するようにしました（shared/_header.html.erb）。

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
-  layout false, only: [ :home ]
-
   def home
     @featured_products = Product.published.featured.limit(10)
     @seasonal_products = Product.published.seasonal.limit(10)
