b25bac4012fb2d015836d9183a13c24a592fbcbe
ezashi <soma3070@icloud.com>
2025-10-26 15:42:39 +0900

    feat: Trixカスタマイズのインポートパスを修正し、削除ボタンのリンクを更新
    - application.js: Trixカスタマイズのインポートパスを修正しました。
    - edit.html.erb: 削除ボタンをlink_toに変更し、スタイルとデータ属性を更新しました。

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "./controllers/index"
-import "./trix_customization"
+import "trix_customization"
 import "trix"
 import "@rails/actiontext"
 import { createRoot } from 'react-dom/client'
